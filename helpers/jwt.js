56914acd5f4e4ec9769ad6cc38a8f04ad7f3b478
Sarra K <Sarra@Sarra-ASUSExpertBook.(none)>
2022-12-02 13:12:11 +0100

    first commit

---
+++ helpers/jwt.js
@@ -0,0 +1,28 @@
+const  expressJWT  = require('express-jwt')
+
+function authJWT() {
+    const secret = process.env.SECRET_KEY
+    const api = process.env.API_URL
+    return expressJWT({
+        secret,
+        algorithms : ['HS256'],
+        // isRevoked : isRevoked
+    }).unless({
+        path:[
+            { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/serviceOff(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/serviceDem(.*)/ , methods: ['GET', 'OPTIONS'] },
+            `${api}/utilisateur/login`,
+            `${api}/utilisateur/register`,
+        ]
+    })
+}
+
+// http://localhost:5353/public/uploads/user.png-1669813392732.png
+// async function isRevoked(req,payload,done) {
+//     if (!payload.isAdmin) {
+//         done(null,true)
+//     }
+//     done()
+// }
+module.exports = authJWT
---
+++ models/serviceOff.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const serviceOffSchema = mongoose.Schema({
+    description: {
+        type: String,
+        // required: true
+    },
+    titre: {
+        type: String,
+        // required: true
+    },
+    prix: {
+        type: Number,
+        // required: true
+    },
+    domaine: {
+        type: String,
+        // required: true
+    },
+    commentaires: [{
+        type:String,
+    }],
+    tempsService: {
+        type: String,
+        // required: true
+    }
+    
+})
+
+serviceOffSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+serviceOffSchema.set('toJSON', {
+    virtuals: true,
+});
+
+exports.ServiceOff = mongoose.model('ServiceOff', serviceOffSchema);
